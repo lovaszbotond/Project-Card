adae30dabbcf490f38d2426e2b2f09f76ecb809d
lovaszbotond <botond.lovasz@gmail.com>
2020-05-22 13:57:12 +0200

    Set +2 parameter to setPlayer method

--- src/main/java/controller/MainMenuController.java
+++ src/main/java/controller/MainMenuController.java
@@ -61,7 +61,7 @@ public class MainMenuController {
         if (!p1Field.getText().isEmpty() && !p2Field.getText().isEmpty())
         {
             FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/gameplay/gameplaytable.fxml"));
-            GameData.setPLayers(p1Field.getText(),p2Field.getText(),null,null,null,null);
+            GameData.setPLayers(p1Field.getText(),p2Field.getText(),null,null,null,null,0,0);
             Parent root = fxmlLoader.load();
             Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
             stage.setScene(new Scene(root));
