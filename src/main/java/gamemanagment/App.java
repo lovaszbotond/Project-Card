bedb311480a3869353f637306f174092d394e8a6
lovaszbotond <botond.lovasz@gmail.com>
2020-05-22 23:14:47 +0200

    Changed the path of fxml

--- src/main/java/gamemanagment/App.java
+++ src/main/java/gamemanagment/App.java
@@ -15,7 +15,7 @@ public class App extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
 
-        Parent root = FXMLLoader.load(getClass().getResource("/fxml/mainmenu/mainmenu.fxml"));
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("fxml/mainmenu/mainmenu.fxml"));
         primaryStage.setTitle("Project--Card");
         primaryStage.setResizable(false);
         primaryStage.setScene(new Scene(root));
