7dc2402baf214177d16f8f5c14e45991729f930e
lovaszbotond <botond.lovasz@gmail.com>
2020-05-25 21:24:31 +0200

    Create the conrtoller for engame.fxml

---
+++ src/main/java/controller/EndGame.java
@@ -0,0 +1,25 @@
+package controller;
+
+import gameplay.GameData;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import org.tinylog.Logger;
+
+import java.io.IOException;
+
+public class EndGame {
+    public void endGameHandler(ActionEvent actionEvent) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getClassLoader().getResource("fxml/mainmenu/mainmenu.fxml"));
+        Parent root = fxmlLoader.load();
+        Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        stage.setScene(new Scene(root));
+        stage.setTitle("Project--card");
+        stage.setResizable(false);
+        stage.show();
+        Logger.info("The Game is over.");
+    }
+}
