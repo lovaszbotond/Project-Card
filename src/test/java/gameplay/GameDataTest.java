c9436e3f2da635569be537ff685dbfa2de0c70ca
lovaszbotond <botond.lovasz@gmail.com>
2020-05-25 21:26:19 +0200

    Test Sketch for GameData

---
+++ src/test/java/gameplay/GameDataTest.java
@@ -0,0 +1,16 @@
+package gameplay;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GameDataTest {
+
+    @Test
+    void setPLayers() {
+    }
+
+    @Test
+    void setTurn() {
+    }
+}
