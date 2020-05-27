0485e0ff0e0057bcdbf11f03c22ee75f051ec6e2
lovaszbotond <botond.lovasz@gmail.com>
2020-05-27 16:33:39 +0200

    Gramatical repair

--- README.md
+++ README.md
@@ -4,12 +4,12 @@
 
 ## Description
         This Game is a basic 1v1 card game. There are two players on the board.
-        They can draw cards and both of them got one special skill. They can
+        They can draw cards and both of them get one special skill. They can
         use it only once in the game.
-        Cards in the card deck has got attack and defense points.
-        Player can lose health point by attack and defense as well.
-        The aim of the game is get the other player health point 0 or less.
-        Players can hold maximum two cards in the hand , and on the table.
+        Cards in the card deck have attack and defense points.
+        Player can lose health points by attack and defense as well.
+        The aim of the game is to get the other player health points 0 or less.
+        Players can hold maximum two cards in the hand, and two on the table.
         For more information please read the rules before you start the game.
 
 ## Requirements of the project
--- src/test/java/gameplay/GameDataTest.java
+++ src/test/java/gameplay/GameDataTest.java
@@ -1,16 +1,72 @@
 package gameplay;
 
+import gameplay.gamecards.CardDeck;
+import gameplay.gamecards.GameCard;
+import lombok.Builder;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class GameDataTest {
 
+
     @Test
-    void setPLayers() {
+    void setPLayers()
+    {
+
+        List <GameCard> gameCards = new ArrayList<>();
+        CardDeck cardDeck = new CardDeck(gameCards);
+        gameCards.add(new GameCard("k1", "", 13,10124));
+        gameCards.add(new GameCard("k2", "", 11,10123));
+        gameCards.add(new GameCard("k3", "", 123,10123));
+        gameCards.add(new GameCard("k4", "", 123,10123));
+        gameCards.add(new GameCard("k5", "", 123,10123));
+        gameCards.add(new GameCard("k6", "", 123,10123));
+        gameCards.add(new GameCard("k7", "", 123,10123));
+        gameCards.add(new GameCard("k8", "", 123,10123));
+        gameCards.add(new GameCard("k9", "", 123,10123));
+        gameCards.add(new GameCard("k10", "", 123,10123));
+
+        GameData.setPLayers("scorpion",null,cardDeck,cardDeck,null,null);
+
+        assertNotEquals(20,GameData.getGamePlayer(0).getHealthpoint());
+        assertEquals(50,GameData.getGamePlayer(0).getHealthpoint());
+
+        assertNotEquals(20,GameData.getGamePlayer(1).getHealthpoint());
+        assertEquals(50,GameData.getGamePlayer(1).getHealthpoint());
+
+        assertNull(GameData.getGamePlayer(0).getSpecial());
+        assertNull(GameData.getGamePlayer(1).getSpecial());
+
+        assertEquals(cardDeck,GameData.getGamePlayer(0).getCarddeck());
+        assertEquals(cardDeck,GameData.getGamePlayer(1).getCarddeck());
+
+        assertEquals("scorpion",GameData.getGamePlayer(0).getName());
+        assertEquals(null,GameData.getGamePlayer(1).getName());
+
+        assertNotEquals("null",GameData.getGamePlayer(0).getName());
+        assertNotEquals("scorpion",GameData.getGamePlayer(1).getName());
     }
 
     @Test
     void setTurn() {
+
+        GameData.setTurn(0);
+
+        assertEquals(0,GameData.getTurn());
+        assertNotEquals(1,GameData.getTurn());
+
+        GameData.setTurn(1);
+
+        assertEquals(1,GameData.getTurn());
+        assertNotEquals(0,GameData.getTurn());
+
+        GameData.setTurn(2);
+
+        assertNotEquals(2,GameData.getTurn());
+
     }
 }
